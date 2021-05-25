41743fc5fd02ff284b0162eccc4d3b08c6ae98d5
coldboya123 <65642613+coldboya123@users.noreply.github.com>
2021-05-25 10:17:53 +0700

    Initial Commit

---
+++ Practice_Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_Navigation
+//
+//  Created by Nguyen Duc Huy on 5/25/21.
+//  Copyright © 2021 Nguyen Duc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
